bf6b567872a3fe7c6d15d2a35d94a634b8c56c3f
tkdgh7063 <hoo412298@gmail.com>
2025-04-15 01:00:52 +0900

    Add CSS modules for App and Button components
    Created App.module.css and Button.module.css to apply scoped styles using CSS Modules.
    Updated corresponding component files to import and use the module styles.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Let's get start!!</h1>
+      <h1 className={styles.title}>Let's get started!!</h1>
+      <Button text="Button" />
     </div>
   );
 }
